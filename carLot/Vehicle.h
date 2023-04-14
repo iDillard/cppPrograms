08dd2efee5e2ac24cb3f69778379ea82bd234094
Isaiah Dillard <96638505+iDillard@users.noreply.github.com>
2023-04-14 18:23:19 -0400

    Rename and Update Code
    There was a conflict with the file name.
    Updated code errors there were not present because of the conflict with the file name.

--- carLot/Vehicle.h
+++ carLot/Vehicle.h
@@ -3,40 +3,9 @@
 #include <string>
 
 using namespace std;
-using std::string;
 
 class Vehicle
 {
-
-public:
-    // Getters
-    Vehicle(string vMake, string vModel, int vYear, double vMileage);
-
-    int getVehicleYear();
-
-    double getVehicleMileage(); // get the vehicle milage
-
-    string getVehicleMake(); // get vehicle make
-
-    string getVehicleModel(); // get vehicle model
-
-    string getVehicleColor(); // get vehicle color
-
-    // Setters
-    void setVehicleMileage(); // set mileage
-
-    void setVehicleMake(); // set vehicle make
-
-    void setVehicleModel(); // set vehicle model
-
-    void setVehicleColor(); // set vehicle color
-
-    // Constructor
-    Vehicle();
-
-    // Constructor with parameters.
-    Vehicle(string vMake, string vModel, int vYear, double vMileage);
-
 private:
     double year;
     double mileage;
@@ -44,4 +13,24 @@ private:
     string make;
     string model;
     string color;
+
+public:
+    // Getters
+    Vehicle(string vMake, string vModel, int vYear, double vMileage);
+    int getVehicleYear();
+    double getVehicleMileage(); // get the vehicle milage
+    string getVehicleMake();    // get vehicle make
+    string getVehicleModel();   // get vehicle model
+    string getVehicleColor();   // get vehicle color
+
+    // Setters
+    void setVehicleMileage(double vMileage); // set mileage
+    void setVehicleMake(string vMake);       // set vehicle make
+    void setVehicleModel(string vModel);     // set vehicle model
+    void setVehicleYear(double vYear);
+    void setVehicleColor(string vColor); // set vehicle color
+
+    // Constructor
+    Vehicle();
+    Vehicle(string vMake, string vModel, string vColor, int vYear, double vMileage);
 };
